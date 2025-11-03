a8afc4585e4c2264ca9a1586102c931c9c0cc7c6
Yuvaraj7744 <yr307761@gmail.com>
2025-11-03 21:34:50 +0530

    first commit

--- src/Assesments/Slick.jsx
+++ src/Assesments/Slick.jsx
@@ -39,12 +39,14 @@ const Slick = () => {
         breakpoint:1024,
         settings:{
           slidesToShow:1,
+          slidesToShow:1,
         },
       },
            {
-        breakpoint:425,
+        breakpoint:640,
         settings:{
           slidesToShow:1,
+          slidesToScroll:1,
         },
       },
     ]
