5b2ec1073edf35e12d577aa1af56335a2fb50139
Yuvaraj7744 <yr307761@gmail.com>
2025-11-03 21:55:45 +0530

    first commit

--- src/Assesments/Slick.jsx
+++ src/Assesments/Slick.jsx
@@ -35,18 +35,11 @@ const Slick = () => {
     arrows:true,
  
     responsive:[
-      {
-        breakpoint:1024,
-        settings:{
-          slidesToShow:1,
-          slidesToShow:1,
-        },
-      },
+      
            {
-        breakpoint:640,
+        breakpoint:768,
         settings:{
           slidesToShow:1,
-          slidesToScroll:1,
         },
       },
     ]
--- src/Assesments/Specialist.jsx
+++ src/Assesments/Specialist.jsx
@@ -81,8 +81,7 @@ const Specialist = () => {
     slidesToScroll: 1,
     arrows:false,
     responsive: [
-      { breakpoint: 1024, settings: { slidesToShow: 2 } },
-      { breakpoint: 640, settings: { slidesToShow: 1 } },
+      { breakpoint: 768, settings: { slidesToShow: 1 } },
     ],
   };
 
